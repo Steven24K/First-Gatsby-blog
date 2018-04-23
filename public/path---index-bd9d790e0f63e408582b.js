814ee0c394b3dd07f65ce525312c40722fdd872b
Steven <s.koerts2@gmail.com>
2018-04-23 13:56:30 +0200

    remvove public folder

--- public/path---index-bd9d790e0f63e408582b.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0x81b8806e4260],{524:function(t,a){t.exports={data:{site:{siteMetadata:{title:"My first Gatsby blog"}}},pathContext:{}}}});
-//# sourceMappingURL=path---index-bd9d790e0f63e408582b.js.map
