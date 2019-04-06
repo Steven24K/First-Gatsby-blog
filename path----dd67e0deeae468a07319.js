d75f8504c40797212e20287f6c416bd469e32ff1
Steven <s.koerts2@gmail.com>
2019-04-06 20:10:48 +0200

    Updates

--- path----dd67e0deeae468a07319.js
+++ path----dd67e0deeae468a07319.js
@@ -1,2 +1,2 @@
-webpackJsonp([60335399758886],{163:function(t,a){t.exports={data:{site:{siteMetadata:{title:"My first Gatsby blog"}}},layoutContext:{}}}});
+webpackJsonp([60335399758886],{104:function(t,a){t.exports={data:{site:{siteMetadata:{title:"My first Gatsby blog"}}},layoutContext:{}}}});
 //# sourceMappingURL=path----dd67e0deeae468a07319.js.map
--- path---about-bd9d790e0f63e408582b.js
+++ path---about-bd9d790e0f63e408582b.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xf927f8900006],{522:function(t,a){t.exports={data:{site:{siteMetadata:{title:"My first Gatsby blog"}}},pathContext:{}}}});
+webpackJsonp([0xf927f8900006],{354:function(t,a){t.exports={data:{site:{siteMetadata:{title:"My first Gatsby blog"}}},pathContext:{}}}});
 //# sourceMappingURL=path---about-bd9d790e0f63e408582b.js.map
